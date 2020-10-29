8f332f0f0f9a7d9bb002956ae0f839c0b81604d0
Natalia Shulzhenko <savostyuk@gmail.com>
2020-10-29 12:49:14 +0300

    add tr Headings, change style

--- ishop/IShop.js
+++ ishop/IShop.js
@@ -12,7 +12,16 @@ return {shop:'Название не указано'}
     },
 
 render: function (){
-        var goodsCode = this.props.goods.map( v =>
+    var headings = Object.keys(this.props.goods[0]);
+    for (var z = 0; z < headings.length; z++){
+        if (headings[z] === "code") headings.splice(z, 1); //убираем лишние элементы-пустые строки
+    }
+
+    var headingsCode = headings.map(function (k){
+        return React.DOM.td ({key:k,className: 'HeadingsItem' }, k)
+    })
+
+            var goodsCode = this.props.goods.map( v =>
             React.DOM.tr ({key:v.code, className: 'Item'},
                 React.DOM.td ({className:'Name'}, v.name),
                 React.DOM.td ({className:'Price'}, v.price),
@@ -25,7 +34,8 @@ render: function (){
     return React.DOM.div( {className:'IShop'},
                React.DOM.div( {className:'Shop'}, this.props.shop ),
                React.DOM.table ( {className:'GoodsShop'},
-                   React.DOM.tbody({}, goodsCode)),
+                   React.DOM.tbody({},
+                       React.DOM.tr ({className: 'Headings'}, headingsCode),  goodsCode)),
     );
 
 },
