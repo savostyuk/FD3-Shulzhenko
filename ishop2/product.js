6c2a90b59a7391c7c9601269b157163ebf497436
Natalia Shulzhenko <savostyuk@gmail.com>
2020-11-11 23:25:16 +0300

    add callback

--- ishop2/product.js
+++ ishop2/product.js
@@ -3,49 +3,36 @@ var Product = React.createClass ({
     displayName: Product,
 
     propTypes: {
+        code: React.PropTypes.number.isRequired,
         name: React.PropTypes.string.isRequired,
         price: React.PropTypes.string.isRequired,
         url: React.PropTypes.string.isRequired,
         balance: React.PropTypes.string.isRequired,
+        classNameItem: React.PropTypes.string.isRequired,
+        cbSelectItem:React.PropTypes.func.isRequired,
+        cbDeleteItem:React.PropTypes.func.isRequired,
     },
 
-    getInitialState: function(){
-        return {
-
-            selectedItemCode: 0,
-            classNameItem: 'Item',
-        }
-
-    },
-
-    colorTR(){
-        var classNameItem;
-        if (this.state.selectedItemCode=this.props.code)
-            classNameItem = 'Item_Select'
-        else
-            classNameItem = 'Item';
-        this.setState ({ classNameItem: classNameItem});
-  },
 
     highlightItem(EO){
-        this.setState({selectedItemCode: this.props.code}, this.colorTR)
+        this.props.cbSelectItem ( this.props.code);
     },
 
-    deleteItem(EO){
-
+    delItem(EO){
+        EO.stopPropagation();
+        this.props.cbDeleteItem ( this.props.code);
     },
 
     render: function (){
 
-console.log(this.props.code);
-        return React.DOM.tr ({className: this.state.classNameItem, onClick:this.highlightItem},
+        return React.DOM.tr ({className: this.props.classNameItem, onClick:this.highlightItem},
             React.DOM.td ({className:'Name'}, this.props.name),
             React.DOM.td ({className:'Price'}, this.props.price),
             React.DOM.td ({className:'Url'},
                 React.DOM.a ({className:'Img', href:this.props.url},  this.props.url),),
             React.DOM.td ({className:'Balance'}, this.props.balance),
             React.DOM.td ({className:'Control'},
-                React.DOM.input ({type:'button', value:'Delete', onClick:this.deleteItem})),
+                React.DOM.input ({type:'button', value:'Delete', onClick:this.delItem})),
             )
 
     },
--- ishop2/shop.js
+++ ishop2/shop.js
@@ -7,18 +7,49 @@ var Shop = React.createClass ({
         goods: React.PropTypes.array.isRequired,
     },
 
+    getInitialState: function(){
+        return {
+            goods: this.props.goods.slice(),
+            selectedItemCode: 0,
+            classNameItemDefault: 'Item',
+            classNameItemSelect: 'Item_Select',
+        }
+
+    },
+
+    selectItem(code){
+        this.setState ({selectedItemCode: code })
+    },
+
+    deleteItem(code){
+    var Question = confirm("Вы хотите удалить товар?");
+       if (Question) {
+       var filterGoods = this.state.goods.filter (v =>
+       v.code !==code);
+       this.setState ({ goods:filterGoods})
+}
+    },
+
     render: function (){
         var headings = Object.keys(this.props.goods[0]);
         for (var z = 0; z < headings.length; z++){
             if (headings[z] === "code") headings.splice(z, 1);
-        }
+        };
 
         var headingsCode = headings.map( k =>
             React.DOM.td ({key:k,className: 'HeadingsItem' }, k)
         );
 
-        var goodsCode = this.props.goods.map (v=>
-        React.createElement(Product, { key:v.code, code:v.code, name:v.name, price:v.price, url:v.url, balance:v.balance}))
+        var goodsCode = this.state.goods.map (v=>
+        React.createElement(Product, { key:v.code,
+            code:v.code,
+            name:v.name,
+            price:v.price,
+            url:v.url,
+            balance:v.balance,
+            cbSelectItem: this.selectItem,
+            cbDeleteItem: this.deleteItem,
+        classNameItem: (this.state.selectedItemCode == v.code)?this.state.classNameItemSelect:this.state.classNameItemDefault}))
 
         return React.DOM.div( {className:'IShop'},
             React.DOM.div( {className:'Shop'}, this.props.shop ),
