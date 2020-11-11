4b8728dab55a45a858dbcb0ccdabd8ac1a61bd15
Natalia Shulzhenko <savostyuk@gmail.com>
2020-11-11 18:24:03 +0300

    add function processList

--- filter/FilterBlock.js
+++ filter/FilterBlock.js
@@ -11,34 +11,45 @@ var FilterBlock = React.createClass({
     getInitialState: function(){
         return {
          filtered: '',
-         sortered: false,
-         editText: '',
-         defwords: this.props.words,
+         sorted: false,
+         processingWords: this.props.words,
         }
     },
 
+    processList(){
+      var res = this.props.words.slice();
+      if (this.state.filtered !="")
+          res = res.filter( el =>
+              el.indexOf(this.state.filtered) != -1);
+
+      if (this.state.sorted == true)
+          res.sort();
+
+      this.setState ({ processingWords: res});
+    },
+
     checkboxSorted(EO){
-        this.setState( {sortered: EO.target.checked});
+        this.setState( {sorted: EO.target.checked}, this.processList);
     },
 
     editText(EO) {
-        this.setState( {filtered: EO.target.value});
+        this.setState( {filtered: EO.target.value}, this.processList);
     },
 
     defaultWords(){
-        this.setState ({sortered:false, filtered: '', defwords: this.props.words, });
+        this.setState ({sorted:false, filtered: '', processingWords: this.props.words});
     },
 
     render: function (){
         var OptionList = [];
-        this.state.defwords.forEach(function (v){
+        this.state.processingWords.forEach(function (v){
             var option = React.DOM.option ({multiple:true, classList: 'Filter_Option'},v)
             OptionList.push(option);
-        })
+        });
 
         return React.DOM.div ( {className: 'Filter_Block'},
-            React.DOM.input ({type:'checkbox',className: 'Filter_Checkbox', onClick:this.checkboxSorted}),
-            React.DOM.input ({type:'text', className: 'Filter_Input', onChange:this.editText}),
+            React.DOM.input ({type:'checkbox',className: 'Filter_Checkbox', checked: this.state.sorted, onClick:this.checkboxSorted}),
+            React.DOM.input ({type:'text', className: 'Filter_Input', value: this.state.filtered, onChange:this.editText}),
             React.DOM.input ({type:'button', value:'сброс', className: 'Filter_Button', onClick:this.defaultWords}),
             React.DOM.select ({multiple:true, className: 'Filter_Select'}, OptionList),
         );
