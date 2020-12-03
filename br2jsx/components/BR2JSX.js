270c20f2e2a968f2df08c8ff38b2e1d5a1c6fce4
Natalia Shulzhenko <savostyuk@gmail.com>
2020-12-03 17:18:18 +0300

    update project br

---
+++ br2jsx/components/BR2JSX.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './BR2JSX.css';
+
+class BR2JSX extends React.Component {
+
+    static PropTypes = {
+        text: PropTypes.string.isRequired,
+    }
+
+    render() {
+
+    }
+
+}
