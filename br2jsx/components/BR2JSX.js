983b9d7366af00b94b1cd0adaffa892c0a434943
Natalia Shulzhenko <savostyuk@gmail.com>
2020-12-03 17:53:43 +0300

    update project br

--- br2jsx/components/BR2JSX.js
+++ br2jsx/components/BR2JSX.js
@@ -12,7 +12,23 @@ class BR2JSX extends React.Component {
     }
 
     render() {
+        let regexp = /<br *\/?>/;
+        let words = this.props.text.split(regexp);
+        let parts = [];
+        words.forEach((w,i)=>{
+            if (i)
+          parts.push(<br key={i}/>);
+          parts.push(w);
+        })
 
+        return (
+            <div  className="BR2JSX">
+                {parts}
+            </div>
+        )
     }
-
 }
+
+export default BR2JSX;
+
+
