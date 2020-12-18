0a1430bd5530a8523c7bf7c3a48733701fc7bd57
Natalia Shulzhenko <savostyuk@gmail.com>
2020-12-18 23:24:03 +0300

    update

--- MobileTest/__tests__/MobileClient-test.js
+++ MobileTest/__tests__/MobileClient-test.js
@@ -26,6 +26,27 @@ test ('Работа кнопки Удалить', () => {
     // получаем уже изменённый снэпшот
     componentTree=component.toJSON();
     expect(componentTree).toMatchSnapshot();
+});
+
+test ('Работа кнопки Редактировать', () => {
+
+    // создаём тестовую версию компонента
+    const component = renderer.create(
+        <MobileClient key={client.id} info={client} />
+    );
+
+    // получаем снэпшот (HTML-снимок) компонента для сверки, что вёрстка не испортилась
+    let componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+
+    // найдём в вёрстке компонента саму кнопку
+    const buttonElem = component.root.find( el => el.props.className==='buttonEdit');
+    // и "нажмём" на неё
+    buttonElem.props.onClick();
+
+    // получаем уже изменённый снэпшот
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
 
     // "нажмём" кнопку ещё раз
     buttonElem.props.onClick();
